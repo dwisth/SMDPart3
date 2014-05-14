24ee208e1a05706a8129feea37e725425a1c1203
David Wisth <davidwisth@gmail.com>
2014-05-14 14:36:16 +1000

    Add header and footer

--- ClubBiz/app/models/user.rb
+++ ClubBiz/app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  #attr_protected :firstName, :lastName, :phoneNum, :address
+
 end
--- ClubBiz/config/routes.rb
+++ ClubBiz/config/routes.rb
@@ -7,6 +7,8 @@ ClubBiz::Application.routes.draw do
   get "home/index"
   get "home/about"
   get "home/login"
+
+  root "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
