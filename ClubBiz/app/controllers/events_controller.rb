c957c462ce7d795c576a18e86790eb4a822fae27
wlawi <wlawi@student.unimelb.edu.au>
2014-05-22 09:36:58 +1000

    Association prelim

--- ClubBiz/app/controllers/events_controller.rb
+++ ClubBiz/app/controllers/events_controller.rb
@@ -72,6 +72,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:name, :location, :date, :startTime, :endTime, :description, :membersOnly, :society_id)
+      params.require(:event).permit(:name, :location, :date, :startTime, :endTime, :description, :membersOnly, :society)
     end
 end
